071f430034634e71a85839c7c5b1a8c20289664d
Bkoji1150 <kojibello058@gmail.com>
2022-06-09 12:13:59 -0700

    added

--- template/frontend_app2.sh
+++ template/frontend_app2.sh
@@ -7,4 +7,3 @@ sudo echo '<h1>Welcome to kojitechs - APP-2</h1>' | sudo tee /var/www/html/index
 sudo mkdir /var/www/html/app2
 sudo echo '<!DOCTYPE html> <html> <body style="background-color:rgb(15, 232, 192);"> <h1>Welcome to kojitechs - APP-2</h1> <p>Terraform Demo</p> <p>Application Version: V1</p> </body></html>' | sudo tee /var/www/html/app2/index.html
 sudo curl http://169.254.169.254/latest/dynamic/instance-identity/document -o /var/www/html/app2/metadata.html
-
