51f629ad93e267b04f0f58d0790683458b4100bd
Gerrie Dinga <gerriedinga@Gerries-MacBook-Pro.local>
2022-06-13 21:33:53 -0600

    updates

--- providers.tf
+++ providers.tf
@@ -1,4 +1,23 @@
 
+terraform {
+  required_version = ">=1.1.5"
+
+  backend "s3" {
+      bucket = "ec2-kojitechs-registrationapps-tf-12"
+      dynamodb_table = "terraform-lock"
+      key = "path/env"
+      region = "us-east-1"
+      encrypt = "true"
+  }
+  required_providers {
+      aws = {
+          source = "hashicorp/aws"
+          version = "~> 3.0"
+      }
+  }
+}
+
+
 provider "aws" {
   region  = var.region # 
   profile = "default"
