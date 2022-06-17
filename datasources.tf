852ecd903c9ffb3bfebce89a39b07a747c9faa75
kojitechs <85324828+Bkoji1150@users.noreply.github.com>
2022-06-17 10:12:22 -0700

    Update datasources.tf

--- datasources.tf
+++ datasources.tf
@@ -19,50 +19,50 @@ data "aws_ami" "ami" {
   }
 }
 
-# data "aws_key_pair" "keypair" {
-#   key_name = "kojitech_keypair"
+# # data "aws_key_pair" "keypair" {
+# #   key_name = "kojitech_keypair"
+# # }
+
+# # we'll use data source to pull priv_subnet_id, pub_subnet
+
+# data "aws_vpc" "vpc" {
+#   filter {
+#     name   = "tag:Name"
+#     values = ["kojitechs_vpc"]
+#   }
 # }
 
-# we'll use data source to pull priv_subnet_id, pub_subnet
+# # Pulling down priv_subnet 
+# data "aws_subnet_ids" "priv_subnet" {
+#   vpc_id = local.vpc_id
+#   filter {
+#     name   = "tag:Name"
+#     values = ["priv_*"]
+#   }
+# }
 
-data "aws_vpc" "vpc" {
-  filter {
-    name   = "tag:Name"
-    values = ["kojitechs_vpc"]
-  }
-}
+# data "aws_subnet_ids" "pub_subnet" {
+#   vpc_id = local.vpc_id
+#   filter {
+#     name   = "tag:Name"
+#     values = ["pub_*"]
+#   }
+# }
 
-# Pulling down priv_subnet 
-data "aws_subnet_ids" "priv_subnet" {
-  vpc_id = local.vpc_id
-  filter {
-    name   = "tag:Name"
-    values = ["priv_*"]
-  }
-}
+# # priv_subnet
+# data "aws_subnet" "priv_sub" {
+#   for_each = data.aws_subnet_ids.priv_subnet.ids
+#   id       = each.value
 
-data "aws_subnet_ids" "pub_subnet" {
-  vpc_id = local.vpc_id
-  filter {
-    name   = "tag:Name"
-    values = ["pub_*"]
-  }
-}
+# }
 
-# priv_subnet
-data "aws_subnet" "priv_sub" {
-  for_each = data.aws_subnet_ids.priv_subnet.ids
-  id       = each.value
+# # public_subnet
+# data "aws_subnet" "public_sub" {
+#   for_each = data.aws_subnet_ids.pub_subnet.ids
+#   id       = each.value
+# }
 
-}
-
-# public_subnet
-data "aws_subnet" "public_sub" {
-  for_each = data.aws_subnet_ids.pub_subnet.ids
-  id       = each.value
-}
-
-data "aws_route53_zone" "mydomain" {
-  name = lookup(var.d_name, terraform.workspace)
-}
+# data "aws_route53_zone" "mydomain" {
+#   name = lookup(var.d_name, terraform.workspace)
+# }
 
